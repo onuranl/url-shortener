720f706ac92e54b42d2185d7661a2de883af11f3
onuranl <onuro99cent@gmail.com>
2021-03-24 01:41:04 +0300

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost/urlShortener', {useNewUrlParser: true,  useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/urlShortener', {useNewUrlParser: true,  useUnifiedTopology: true});
 
 app.get('/', async (req, res) => {
     const shortUrls = await shortUrl.find()
